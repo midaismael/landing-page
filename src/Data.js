472cc1a955cf94b52eea6e9efd7b5edc51af2799
Ahmed Ismael <midaismael@gmail.com>
2022-08-22 14:20:39 +0200

    data

---
+++ src/Data.js
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart , faFile , faStar, faPlane} from '@fortawesome/free-solid-svg-icons';
+
+
+const Data = {
+    serv : [
+        {
+            icons: faHeart,
+            title:'Digital Design',
+            text:'Some quick example text to build on the card title and make up the bulk of the cards content. Moltin gives you the platform.'
+        },
+        {
+            icons: faFile,
+            title:'Unlimited Colors',
+            text:'Credibly brand standards compliant users without extensible services. Anibh euismod tincidunt ut laoreet.'
+        },
+        {
+            icons: faPlane,
+            title:'Strategy Solutions',
+            text:'Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean necessary regelialia.'
+        },
+        {
+            icons: faStar,
+            title:'Awesome Support',
+            text:'It is a paradisematic country, in which roasted parts of sentences fly into your mouth leave for the far World.'
+        },
+        {
+            icons: faFile,
+            title:'Truly Multipurpose',
+            text:'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.'
+        },
+        {
+            icons: faHeart,
+            title:'Easy to customize',
+            text:'Question Marks and devious Semikoli, but the Little Blind Text didnt listen. She packed her seven versalia.'
+        }
+    ]
+}
+export default Data;
