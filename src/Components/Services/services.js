0afb0f3f046ab178d68de5f970a6c8ccec61c5ee
Ahmed Ismael <midaismael@gmail.com>
2022-08-22 18:04:45 +0200

    secvices

--- src/Components/Services/services.js
+++ src/Components/Services/services.js
@@ -2,6 +2,7 @@ import React from "react";
 import Data from "../../Data";
 import Servitem from "./Servitem";
 import "./services.css";
+import mobimg from '../../assets/online-world-ba029cf8887f4299446296263ab79b61.svg'
 
 const Services = () => {
     const item = Data.serv.map((itembox)=>{
@@ -25,6 +26,24 @@ const Services = () => {
                 </div>
                 <div className="row">
                     {item}
+                </div>
+                <div className="row lastserv">
+                    <div className="col-md-6">
+                        <h3>A digital web design studio creating modern & engaging online experiences</h3>
+                        <p>Separated they live in Bookmarksgrove right at the coast of the 
+                            <br/>Semantics, a large language ocean. A small river named Duden
+                            <br/>flows by their place and supplies it with the necessary regelialia.</p>
+                            <ul>
+                                <li>We put a lot of effort in design.</li>
+                                <li>The most important ingredient of successful website.</li>
+                                <li>Sed ut perspiciatis unde omnis iste natus error sit.</li>
+                                <li>Submit Your Orgnization.</li>
+                            </ul>
+                            <button>Learn More</button>
+                    </div>
+                    <div className="col-md-6">
+                        <img src={mobimg} alt="" />
+                    </div>
                 </div>
             </div>
         </section>
